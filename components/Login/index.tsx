c550a4edf9577c30903862ac13d0728f05a67880
Maksim V <maks112v@gmail.com>
2020-09-22 10:36:51 -0400

    Update index.tsx

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,22 +1,26 @@
 import firebase from 'firebase';
 import { Container } from '../../styles/Container';
 import Button from '../Button';
+import Seo from '../Seo';
 
 export default function Login() {
   return (
-    <Container>
-      <div>
-        <h3>Login</h3>
-      </div>
-      <Button
-        onClick={() => {
-          firebase
-            .auth()
-            .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
-        }}
-      >
-        Login with Google
-      </Button>
-    </Container>
+    <>
+      <Seo titles={['Login']} />
+      <Container>
+        <div>
+          <h3>Login</h3>
+        </div>
+        <Button
+          onClick={() => {
+            firebase
+              .auth()
+              .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+          }}
+        >
+          Login with Google
+        </Button>
+      </Container>
+    </>
   );
 }
