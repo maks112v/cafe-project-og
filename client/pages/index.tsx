f520e9dbda67e3fcfa3ff1b07933768e7b9e24b0
Maksim V <maks112v@gmail.com>
2021-01-19 10:08:01 -0500

    changed structure

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,76 +1,9 @@
-import styled from '@emotion/styled';
-import { useStore } from '@hooks/store';
-import Link from 'next/link';
-import OrderItem from '../components/OrderItem';
-import Orders from '../components/Orders';
-import Seo from '../components/Seo';
-import { Container } from '../styles/Container';
+import { FunctionComponent } from 'react';
 
-const CardWrapper = styled.div({
-  display: 'grid',
-  gap: 20,
-  gridTemplateColumns: `repeat(auto-fill, minmax(200px, 1fr))`,
-});
+interface Props {}
 
-const ItemCard = styled.div({});
+const IndexPage: FunctionComponent<Props> = ({ children }) => {
+  return <></>;
+};
 
-function Home({ items }) {
-  const { selectableDrinks } = useStore();
-
-  return (
-    <>
-      <Seo titles={['Order']} />
-      <Container>
-        <Orders />
-        <h1>New Order</h1>
-        <p>Click on a drink to place an order.</p>
-        <CardWrapper>
-          {selectableDrinks
-            ?.filter((data) => data?.available)
-            ?.map((data) => (
-              <Link key={data?.id} href={`/orders/new/${data?.id}`}>
-                <OrderItem
-                  {...data}
-
-                  // onClick={() => setSelectedDrink(data?.id)}
-                />
-              </Link>
-            ))}
-        </CardWrapper>
-      </Container>
-    </>
-  );
-}
-
-// export async function getStaticProps(context) {
-//   try {
-//     const query = await firebase.firestore().collection('items').get();
-
-//     let items = [];
-
-//     query.docs.forEach((item) => {
-//       items.push({ id: item.id, ...item.data() });
-//     });
-
-//     return {
-//       props: {
-//         items,
-//       }, // will be passed to the page component as props
-//       revalidate: 30,
-//     };
-//   } catch (err) {
-//     console.log(err);
-
-//     return {
-//       props: {
-//         err: {
-//           // status: err?.status,
-//           message: err?.message,
-//         },
-//       }, // will be passed to the page component as props
-//       revalidate: 1,
-//     };
-//   }
-// }
-
-export default Home;
+export default IndexPage;
